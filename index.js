f38b8806817aa41871dd4d4c17452133603d29f8
sbrookelynn <brookelynn909@gmail.com>
2022-03-30 12:27:53 -0700

    Deleted nav-open

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+    document.body.classList.toggle('');
 });
 
 navLinks.forEach(link => {
